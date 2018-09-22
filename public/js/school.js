f5df8e4668a3dda74b61d4b38fbdacebe4de970d
EC2 Default User <ec2-user@ip-172-31-31-223.us-east-2.compute.internal>
2018-09-22 20:15:56 +0000

    Linked up school search

--- public/js/school.js
+++ public/js/school.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
-  
+  function searchSchool(var searchTarget) {
+    $.ajax({
+      type: 'get',
+      cashe: false,
+      url: "http://map.toronto.ca/geoservices/rest/search/rankedsearch?searchString="+searchTarget+"&searchArea=*&matchType=1&projectionType=2&retRowLimit=50",
+      success: function(data) {
+        console.log(data);
+      }
+    });
+  }
 });
